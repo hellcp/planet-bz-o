37ca80b96c66cbe61fb95b839b4997daa3617460
Stasiek Michalski <stasiek@michalski.cc>
2020-06-08 14:35:48 +0200

    Fix the tests

--- tests/feedcheck.rb
+++ tests/feedcheck.rb
@@ -1,28 +1,17 @@
 require 'iniparser'
 require 'faraday'
 require 'nokogiri'
+require 'uri'
 
 hash = INI.load_file( 'planet.ini' )
-av_dir = 'hackergotchi'
-
-avatars = []
 
 hash.each do |key, section|
   if section.is_a?(Hash)
     print ":: #{key} =>  "
     feed = section['feed'] if section.has_key?('feed')
-    avatar = section['avatar'] if section.has_key?('avatar')
     url_arr = []
     url_arr << section['link'] if section.has_key?('link')
     url_arr << feed if feed
-    # Check if avatar exists
-    if avatar.include? '//'
-      url_arr << avatar
-    else
-      abort("✗\nAvatar not found: hackergotchi/#{avatar}") unless File.file?("#{av_dir}/#{avatar}")
-      avatars << avatar
-    end if avatar
-    print '✓ '
     # Check if URLs return 200 status
     url_arr.each do |url|
       res = Faraday.get(URI(url))
@@ -38,9 +27,3 @@ hash.each do |key, section|
     puts '✓ '
   end
 end
-
-avatars << "default.png"
-avatars.uniq!
-hackergotchis = Dir.foreach(av_dir).select { |f| File.file?("#{av_dir}/#{f}") }
-diff = (hackergotchis - avatars).sort
-abort "There are unused files in hackergotchis:\n#{diff.join(', ')}" unless diff.empty?
